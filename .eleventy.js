28e8bff01d6849eef2a044127e063cb614a0e235
Mike Stilllling <mike@stillingdigital.com>
2024-08-16 10:26:53 -0500

    clean

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,10 @@
 module.exports = (eleventyConfig) => {
-    eleventyConfig.addWatchTarget("dist/assets/css/main.css");
     return {
         dir: {
             input: "src",
             output: "dist",
-            includes: "_includes",
-            data: "_data",
+            includes: "src/_includes",
+            data: "src/_data",
         }
     }
 };
